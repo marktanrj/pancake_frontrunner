8819dadc8e4e04923dc392240d405c693910bc5f
marktanrj <marktanrj@gmail.com>
2021-06-02 23:38:41 +0800

    update

--- src/utils/setAmountApproveWbnb.ts
+++ src/utils/setAmountApproveWbnb.ts
@@ -4,14 +4,17 @@ import { provider, wallet as walletInstance } from "../Instances/Instances";
 
 const WBNB_ADDRESS = config.mainnet_addresses.WBNB;
 const wbnbAbi = config.mainnet_abis.WBNB;
+const PANCAKE_V2_ROUTER_ADDRESS = config.mainnet_addresses.PANCAKE_V2_ROUTER;
+const pancakeV2RouterAbi = config.mainnet_abis.PANCAKE_V2_ROUTER;
 
 const wbnbInstance = new ethers.Contract(WBNB_ADDRESS, wbnbAbi, provider);
+const routerInstance = new ethers.Contract(PANCAKE_V2_ROUTER_ADDRESS, pancakeV2RouterAbi, provider);
 
 export const setAmountApproveWbnb = async (bnbToApprove: string) => {
   const wbnbConnected = wbnbInstance.connect(walletInstance);
   const amountIn = ethers.utils.parseEther(bnbToApprove);
-  const amountInDeposit = amountIn;
-  const txOutput = await wbnbConnected.deposit({ value: amountInDeposit });
+  const amountToApprove = amountIn;
+  const txOutput = await wbnbConnected.approve(routerInstance.address, amountToApprove); //set once
   return txOutput;
 };
 
